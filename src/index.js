4193517085f134680b20a9bfabf8c9a6a174bc7a
Alisa Gagina <alisa.gagina@mail.mcgill.ca>
2021-02-13 19:36:43 -0500

    added dynamic markers via checkbox selection

--- src/index.js
+++ src/index.js
@@ -17,110 +17,135 @@
       const features = [
         {
           position: new google.maps.LatLng(49.25721, -123.1963),
-          type: "info",
+          type: "fountain",
         },
         {
           position: new google.maps.LatLng(49.25539, -123.1982),
-          type: "info",
+          type: "fountain",
         },
         {
           position: new google.maps.LatLng(49.25747, -123.19252),
-          type: "info",
+          type: "fountain",
         },
         {
           position: new google.maps.LatLng(49.2525, -123.19907),
-          type: "info",
+          type: "fountain",
         },
         {
           position: new google.maps.LatLng(49.25725, -123.23011),
-          type: "info",
+          type: "fountain",
         },
         {
           position: new google.maps.LatLng(49.25872, -123.23089),
-          type: "info",
+          type: "fountain",
         },
         {
           position: new google.maps.LatLng(49.25784, -123.23094),
-          type: "info",
+          type: "fountain",
         },
         {
           position: new google.maps.LatLng(49.25682, -123.23149),
-          type: "info",
+          type: "fountain",
         },
         {
           position: new google.maps.LatLng(49.2579, -123.23463),
-          type: "info",
+          type: "fountain",
         },
         {
           position: new google.maps.LatLng(49.25666, -123.23468),
-          type: "info",
+          type: "fountain",
         },
         {
           position: new google.maps.LatLng(49.256988, -123.23364),
-          type: "info",
+          type: "fountain",
         },
         {
           position: new google.maps.LatLng(49.25662347903106, -123.19879464019775),
-          type: "parking",
+          type: "washroom",
         },
         {
           position: new google.maps.LatLng(49.256365282092855, -123.19937399734496),
-          type: "parking",
+          type: "washroom",
         },
         {
           position: new google.maps.LatLng(49.25665018901448, -123.1982474695587),
-          type: "parking",
+          type: "washroom",
         },
         {
           position: new google.maps.LatLng(49.259543720969806, -123.23112279762267),
-          type: "parking",
+          type: "washroom",
         },
         {
           position: new google.maps.LatLng(49.25608037421864, -123.23288232673644),
-          type: "parking",
+          type: "washroom",
         },
         {
           position: new google.maps.LatLng(49.25851096325805, -123.2344058214569),
-          type: "parking",
+          type: "washroom",
         },
         {
           position: new google.maps.LatLng(49.25818154739766, -123.2346203981781),
-          type: "parking",
+          type: "washroom",
         },
         {
           position: new google.maps.LatLng(49.25727341958453, -123.23348314155578),
-          type: "library",
+          type: "park",
         },
       ];
     
+    const markers=[];
       map = new google.maps.Map(document.getElementById('map'), mapOptions);
       // Create markers.
+
+        getColour=function(category){
+            if(category=='fountain'){
+                return {
+                    url: "https://cdn.iconscout.com/icon/free/png-256/water-glass-1440342-1216076.png",
+                    scaledSize: new google.maps.Size(50, 50), // scaled size
+                  }
+            }else if(category=='washroom'){
+                    return {
+                        url: "https://img.icons8.com/pastel-glyph/2x/toilet.png",
+                        scaledSize: new google.maps.Size(50, 50), // scaled size
+
+                      }
+            }
+            else if(category=='park'){
+                return {
+                    url: "https://cdn.iconscout.com/icon/free/png-256/paw-29-459421.png",
+                    scaledSize: new google.maps.Size(50, 50), // scaled size
+                  }
+        }
+        }
+
         for (let i = 0; i < features.length; i++) {
             const marker = new google.maps.Marker({
             position: features[i].position,
             map: map,
+            visible:true,
+            type: features[i].type,
+            icon: getColour(features[i].type)
             });
+            markers[i]=marker;
+            
         }
 
-  }
-  
 
-  
-  /**
-   * Function to filter markers by category
-   */
-  
-  filterMarkers = function (category) {
-      for (i = 0; i < gmarkers1.length; i++) {
-          marker = gmarkers1[i];
+    filterMarkers = function (category, checked) {
+      for (i = 0; i < markers.length; i++) {
+          marker = markers[i];
           // If is same category or category not picked
-          if (marker.category == category || category.length === 0) {
-              marker.setVisible(true);
-          }
-          // Categories don't match 
-          else {
-              marker.setVisible(false);
+          if (marker.type == category) {
+              if(checked){
+                  marker.setVisible(true);
+              }else{
+                marker.setVisible(false);
+              }             
           }
       }
   }
+  
+
+  }
+  
   
