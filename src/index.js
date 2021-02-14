9b10e695221b2781caed0c3974065df4d9be4262
Alisa Gagina <alisa.gagina@mail.mcgill.ca>
2021-02-14 01:57:27 -0500

    loaded dataset onto frontend

--- src/index.js
+++ src/index.js
@@ -19,89 +19,122 @@
         return new google.maps.LatLng(lat, lng)
     }
 
-    
-    const features = [
-      {
-        lat:49.25721, lng :-123.1963,
-        type: "fountain", category:{in_operation:"na", pet_friendly:'na'},
-      },
-      {
-        lat:49.25711, lng :-123.263,
-        type: "fountain", category:{in_operation:"na", pet_friendly:'na'},
-      },
-      {
-        lat:49.45721, lng :-123.3963,
-        type: "fountain", category:{in_operation:"na", pet_friendly:'na'},
-      },
-      {
-        lat:48.45721, lng :-124.3963,
-        type: "fountain", category:{in_operation:"na", pet_friendly:'na'},
-      },
-      {
-        lat:48.45721, lng :-124.3963,
-        type: "fountain", category:{in_operation:"na", pet_friendly:'yes'},
-      },
-      {
-        lat:49.1358, lng :-123.9963,
-        type: "fountain", category:{in_operation:"na", pet_friendly:'yes'},
-      },
-      {
-        lat:48.3268, lng :-123.0963,
-        type: "fountain", category:{in_operation:"yearround", pet_friendly:'yes'}
-      },
-      {
-        lat:49.135, lng :-123.93,
-        type: "fountain", category:{in_operation:"yearround", pet_friendly:'no'}
-      },
-      {
-        lat:49.36, lng :-123.93,
-        type: "fountain", category:{in_operation:"yearround", pet_friendly:'yes'}
-      },
-      {
-        lat:49.3467, lng :-123.93,
-        type: "fountain", category:{in_operation:"may-october", pet_friendly:'no'}
-      },
-      {
-        lat:49.124, lng :-123.93,
-        type: "fountain", category:{in_operation:"may-october", pet_friendly:'yes'}
-      },
-      {
-        lat:49.3416, lng :-122.93,
-        type: "washroom",  category:{hours:"na"},
-      },
-      {
-        lat:49.5641, lng :-123.96,
-        type: "washroom", category:{hours:"na"},
-      },
-      {
-        lat:49.32131, lng :-123.99,
-        type: "washroom", category:{hours:"na"},
-      },
-      {
-        lat:49.34251, lng :-123.98,
-        type: "washroom", category:{hours:"na"},
-      },
-      {
-        lat:49.3821, lng :-123.97,
-        type: "washroom", category:{hours:"na"},
-      },
-      {
-        lat:49.3731, lng :-123.93,
-        type: "washroom", category:{hours:"na"},
-      },
-      {
-        lat:49.3722, lng :-123.92,
-        type: "washroom", category:{hours:"24hrs"},
-      },
-      {
-        coords: [
-            { lat: 49.25608, lng: -123.23 },
-            { lat: 49.24607, lng:-123.24 },
-            { lat: 49.25606, lng: -123.13 },
-          ],
-        type: "dogpark", category:{},
-      },
-    ];
+    function getWaterData(){
+        const url = "https://opendata.vancouver.ca/api/records/1.0/search/?dataset=drinking-fountains&q=&rows=1000&facet=in_operation&facet=pet_friendly&facet=geo_local_area";
+        const request = new XMLHttpRequest();
+        request.open("GET", url, false);
+        request.send(null);
+
+
+        var data = request.responseText;
+        var obj = JSON.parse(data);
+
+        var waterfountains = []
+        
+        for(i=0;i<obj.records.length;i++){
+            var record = obj.records[i];
+            var long = record.fields.geom.coordinates[0];
+            var lat = record.fields.geom.coordinates[1];
+            var type = "fountain";
+            var pet_friendly = "unknown";
+            var in_operation = "na";
+            if ("pet_friendly" in record.fields){
+                pet_friendly = record.fields.pet_friendly;
+            }
+            if ("in_operation" in record.fields){
+                in_operation = record.fields.in_operation;
+            }
+            var waterfountain = {"lat": lat, "lng": long, "type": "fountain", "category":{"in_operation":in_operation, "pet_friendly": pet_friendly}}
+            waterfountains.push(waterfountain);
+            //console.log(waterfountain);
+        }
+        return waterfountains;
+    }
+    //
+
+    function getWashroomData(){
+        const url = "https://opendata.vancouver.ca/api/records/1.0/search/?dataset=public-washrooms&q=&rows=1000&facet=type&facet=summer_hours&facet=winter_hours&facet=wheel_access&facet=maintainer&facet=geo_local_area";
+
+        console.log(url);
+        const request = new XMLHttpRequest();
+        request.open("GET", url, false);
+        request.send(null);
+
+
+        var data = request.responseText;
+        var obj = JSON.parse(data);
+
+        var washrooms = []
+        
+        for(i=0;i<obj.records.length;i++){
+            var record = obj.records[i];
+            var long = record.fields.geom.coordinates[0];
+            var lat = record.fields.geom.coordinates[1];
+            var type = "washroom";
+            var hours;
+            switch (record.fields.winter_hours){
+                case "Dawn to Dusk":
+                    hours = "Dawn to Dusk";
+                    break;
+                case "24 hrs":
+                    hours = "24 hrs";
+                    break;
+                case "Closed":
+                    hours = "closed";
+                    break;
+                default:
+                    hours ="na";
+            }
+            var washroom = {"lat": lat, "lng": long, "type": "washroom", "category":{"hours":hours}};
+            washrooms.push(washroom);
+            //console.log(washroom);
+        }
+        //console.log(washrooms.length);
+        return washrooms;
+    }
+
+    function getDogparkData(){
+        const url = "https://opendata.vancouver.ca/api/records/1.0/search/?dataset=dog-off-leash-parks&q=&rows=1000&facet=geo_local_area";
+
+        console.log(url);
+        const request = new XMLHttpRequest();
+        request.open("GET", url, false);
+        request.send(null);
+
+
+        var data = request.responseText;
+        var obj = JSON.parse(data);
+
+        var dogparks = []
+        
+        for(i=0;i<obj.records.length;i++){
+            var record = obj.records[i];
+            if (record.fields.geom.coordinates.length == 2){
+                var coords = record.fields.geom.coordinates[1];
+            }
+            else{
+                var coords = record.fields.geom.coordinates[0]
+            }
+
+            points = []
+
+            for (j=0; j<coords.length; j++){
+                points.push({"lat": coords[j][1], "lng": coords[j][0]});
+            }
+
+            var type = "dogpark";
+            var dogpark = {"coords":points, "type": "dogpark", "category":{}};
+            dogparks.push(dogpark);
+            //console.log(dogpark);
+        }
+        //console.log("dogpark length" + dogparks.length);
+        return dogparks;
+    }          
+
+    //getDogparkData();
+    var features=getWaterData();
+    features=features.concat(getWashroomData());
+    features=features.concat(getDogparkData());
 
     const markers = [];
     map = new google.maps.Map(document.getElementById("map"), mapOptions);
@@ -139,13 +172,14 @@
       if (category == "fountain") {
         return {
           url:
-            "https://cdn.iconscout.com/icon/free/png-256/water-glass-1440342-1216076.png",
+            "https://www.freepnglogos.com/uploads/water-drop-png/water-drop-falling-illustration-transparent-png-svg-vector-29.png",
           scaledSize: new google.maps.Size(25, 25), // scaled size
         };
       } else if (category == "washroom") {
         return {
-          url: "https://img.icons8.com/pastel-glyph/2x/toilet.png",
-          scaledSize: new google.maps.Size(25, 25), // scaled size
+        //   url: "https://cdn1.iconfinder.com/data/icons/tools-and-construction-2-6/65/64-512.png",
+        url:"http://planyourwalk.online.s3-website-us-east-1.amazonaws.com/toilet_pin.png",
+          scaledSize: new google.maps.Size(40, 40), // scaled size
         };
       } else if (category == "dogpark") {
         return {
@@ -172,11 +206,11 @@
         const marker = new google.maps.Polygon({
          paths: features[i].coords,
          type: features[i].type,
-        strokeColor: "#F86464",
+        strokeColor: "#6A0DAD",
         strokeOpacity: 0.8,
         strokeWeight: 2,
-        fillColor: "#F86464",
-        fillOpacity: 0.35,
+        fillColor: "#6A0DAD",
+        fillOpacity: 0.6,
         })
         marker.setMap(map);
         markers[i] = marker;
